64808f3817e29e7205226663a2d105a2d037de82
Sheerabdhi <sheerabdhi@gmail.com>
2024-02-10 22:52:31 -0500

    im never trusting git command line again

---
+++ ncssm-online-react/src/components/public-page/settabletimer.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+
+var x = new Date("2024-02-11T23:59:59");
+var y = new Date();
+let startSeconds = Math.abs(x.getTime() - y.getTime())/1000;
+
+class TimeComponentSet extends React.Component {
+  constructor(props) {
+    super(props)
+    console.log(this.props)
+    this.state = {
+      seconds: parseInt(this.props.startSeconds, 10) || 0
+    };
+  }
+
+  tick() {
+    this.setState(state => ({
+      seconds: state.seconds - 1
+    }));
+  }
+
+  componentDidMount() {
+    this.interval = setInterval(() => this.tick(), 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+
+  formatTime(secs) {
+    let days = Math.floor(secs/ 86400)
+    let hours   = Math.floor(secs / 3600) % 24;
+    let minutes = Math.floor(secs / 60) % 60;
+    let seconds = secs % 60;
+    return [days, hours, minutes, seconds]
+        .map(v => ('' + v).padStart(2, '0'))
+        .filter((v,i) => v !== '00' || i > 0)
+        .join(':');
+  }
+
+  render() {
+    return (
+      <div>
+        {this.formatTime(this.state.seconds)}
+      </div>
+    );
+  }
+}
+
+export default TimeComponentSet;
